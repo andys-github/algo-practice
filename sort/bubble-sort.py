d18e4461bca4f8098e29de99f114c2a2af09bd3d
Anindya Dey <andy.ani1992@gmail.com>
2022-04-22 16:47:59 +0530

    add bubble sort

---
+++ sort/bubble-sort.py
@@ -0,0 +1,22 @@
+# ---------------------------------------------
+# B U B B L E   S O R T
+# ---------------------------------------------
+# Time Complexity:
+#   Best Case:   O(n^2)
+#   Avg Case:    O(n^2)
+#   Worst Case:  O(n^2)
+#
+# Space Complexity: O(1)
+# ---------------------------------------------
+
+def bubble_sort(arr):
+  arr_size = len(arr)
+  for i in reversed(range(arr_size)):
+    for j in range(i):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+  return arr
+
+input_arr = [6, 4, 15, 10, -3]
+print('Input Array: ', input_arr)
+print('Sorted Array: ', bubble_sort(input_arr))
