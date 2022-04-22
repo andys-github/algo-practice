52aafa72792137bd72cc573a3fb6f5987118ab83
Anindya Dey <andy.ani1992@gmail.com>
2022-04-22 18:10:39 +0530

    add optimized bubble sort

--- sort/bubble-sort.py
+++ sort/bubble-sort.py
@@ -8,15 +8,45 @@
 #
 # Space Complexity: O(1)
 # ---------------------------------------------
+# NOTE:
+# Best case can be optimized for
+# almost sorted array to O(n)
+# ---------------------------------------------
 
 def bubble_sort(arr):
   arr_size = len(arr)
+  _counter = 0
   for i in range(arr_size):
     for j in range(arr_size - 1):
+      _counter += 1
       if arr[j] > arr[j + 1]:
         arr[j], arr[j+1] = arr[j+1], arr[j]
-  return arr
+  return arr, _counter
 
-input_arr = [6, 4, 15, 10, 13]
+def bubble_sort_optimized(arr):
+  arr_size = len(arr)
+  _counter = 0
+
+  for i in range(arr_size):
+    # to keep track of any swaps
+    # that happended in the previous run
+    no_swaps = True
+
+    for j in range(arr_size - 1):
+      _counter += 1
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+        no_swaps = False  # Swap happened
+    # print(arr)
+    if no_swaps: break # If no swap happened in the previous loop, break
+  return arr, _counter
+
+input_arr = [8, 1, 2, 3, 4, 5, 6, 7]
 print('Input Array: ', input_arr)
-print('Sorted Array: ', bubble_sort(input_arr))
+sorted_arr, counter = bubble_sort(input_arr)
+print('Bubble Sorted Array: ', sorted_arr, 'Loops: ', counter)
+
+input_arr2 = [8, 1, 2, 3, 4, 5, 6, 7]
+print('Input Array: ', input_arr2)
+sorted_arr2, counter2 = bubble_sort_optimized(input_arr2)
+print('Optimized Bubble Sorted Array: ', sorted_arr2, 'Loops: ', counter2)
